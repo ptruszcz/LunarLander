543c8465894a288a5dbd6ba4637613f16def5cb0
ptruszcz <piotr.truszczynski95@gmail.com>
2016-06-20 08:06:05 +0200

    almost final

--- src/parsers/HsParser.java
+++ src/parsers/HsParser.java
@@ -6,12 +6,19 @@ import java.io.*;
 import java.util.Arrays;
 import java.util.Properties;
 
+/**
+ * To jest klasa, która zajmuje się parsowaniem pliku zawierającego najlepsze wyniki(highscores) w celu ich wczytywania i edycji
+ */
 public final class HsParser {
 
+    /** ilość rekordowych wyników dla każdego poziomu trudności */
     private static final int NUMBER_OF_RECORDS = 5;
 
+    /**
+     * metoda ładująca plik konfiguracyjny
+     */
     public static Properties loadProperties() {
-        String path = new File("highscores.properties").getAbsolutePath();
+        String path = new File(".\\resources\\highscores.properties").getAbsolutePath();
         File file = new File(path);
         Properties properties = new Properties();
         InputStream sets;
@@ -24,6 +31,9 @@ public final class HsParser {
         return properties;
     }
 
+    /**
+     * metoda pobierająca jeden rekord z pliku konfiguracyjnego
+     */
     public static String[] getRecord(String diff, String n){
         Properties properties = loadProperties();
         System.setProperty("file.encoding", "UTF-8");
@@ -32,7 +42,9 @@ public final class HsParser {
         record[1] = properties.getProperty(diff+"_POINTS"+n);
         return record;
     }
-
+    /**
+     * metoda pobierająca wszystkie rekordy na poziomie łatwym
+     */
     public static String[] getEasy(){
         String[] easy = new String [NUMBER_OF_RECORDS];
         for(int i=0; i<NUMBER_OF_RECORDS; i++){
@@ -41,7 +53,9 @@ public final class HsParser {
         }
         return easy;
     }
-
+    /**
+     * metoda pobierająca wszystkie rekordy na poziomie średnim
+     */
     public static String[] getMedium(){
         String[] medium = new String [NUMBER_OF_RECORDS];
         for(int i=0; i<NUMBER_OF_RECORDS; i++){
@@ -50,7 +64,9 @@ public final class HsParser {
         }
         return medium;
     }
-
+    /**
+     * metoda pobierająca wszystkie rekordy na poziomie trudnym
+     */
     public static String[] getHard(){
         String[] hard = new String [NUMBER_OF_RECORDS];
         for(int i=0; i<NUMBER_OF_RECORDS; i++){
@@ -59,7 +75,9 @@ public final class HsParser {
         }
         return hard;
     }
-
+    /**
+     * metoda służąca do edycji rekordów i zapisie pliku properties
+     */
     public static void saveProperties(String nameKey, String scoreKey, String name, String score) {
         String path = new File("highscores.properties").getAbsolutePath();
         File file = new File(path);
@@ -74,7 +92,9 @@ public final class HsParser {
             e.printStackTrace();
         }
     }
-
+    /**
+     * metoda zapisująca odpowiedni wynik do pliku konfiguracyjnego
+     */
     public static void setScore(String n, String name, String score){
         saveProperties(Integer.toString(MainFrame.getDifficulty())+"_NAME"+n, Integer.toString(MainFrame.getDifficulty())+"_POINTS"+n, name, score);
     }
--- src/parsers/Parser.java
+++ src/parsers/Parser.java
@@ -7,10 +7,17 @@ import java.io.InputStream;
 import java.util.Arrays;
 import java.util.Properties;
 
+/**
+ * To jest klasa, która zajmuje się parsowaniem pliku konfiguracyjnego w celu budowy poziomów oraz
+ * pobraniem podstawowych wartości dotyczących sterowaniem statku
+ */
 public final class Parser
 {
+    /**
+     * metoda ładująca plik konfiguracyjny
+     */
     public static Properties loadProperties() {
-        String path = new File("config.properties").getAbsolutePath();
+        String path = new File(".\\resources\\config.properties").getAbsolutePath();
         File file = new File(path);
         Properties properties = new Properties();
         InputStream sets;
@@ -23,6 +30,9 @@ public final class Parser
         return properties;
     }
 
+    /**
+     * metoda pobierająca dane o zmiennej, która decyduje o odświeżaniu gry
+     */
     public static int getSpeed(){
         Properties properties = loadProperties();
         System.setProperty("file.encoding", "UTF-8");
@@ -30,7 +40,9 @@ public final class Parser
         int speed = Integer.parseInt(speeds);
         return speed;
     }
-
+    /**
+     * metoda pobierająca liczbę poziomów
+     */
     public static int getLastlevel(){
         Properties properties = loadProperties();
         System.setProperty("file.encoding", "UTF-8");
@@ -38,7 +50,9 @@ public final class Parser
         int lastlevel = Integer.parseInt(lastlevels);
         return lastlevel;
     }
-
+    /**
+     * metoda pobierająca rozdzielczość X
+     */
     public static int getXres(){
         Properties properties = loadProperties();
         System.setProperty("file.encoding", "UTF-8");
@@ -46,7 +60,9 @@ public final class Parser
         int xres = Integer.parseInt(xress);
         return xres;
     }
-
+    /**
+     * metoda pobierająca rozdzielczość Y
+     */
     public static int getYres(){
         Properties properties = loadProperties();
         System.setProperty("file.encoding", "UTF-8");
@@ -54,7 +70,9 @@ public final class Parser
         int yres = Integer.parseInt(yress);
         return yres;
     }
-
+    /**
+     * metoda pobierająca ilość punktów do budowy planszy
+     */
     public static int getPoints(){
         Properties properties = loadProperties();
         System.setProperty("file.encoding", "UTF-8");
@@ -62,7 +80,9 @@ public final class Parser
         int points = Integer.parseInt(pointss);
         return points;
     }
-
+    /**
+     * metoda pobierająca daną odpowiadającą za siłę grawitacji
+     */
     public static double getGravity(){
         Properties properties = loadProperties();
         System.setProperty("file.encoding", "UTF-8");
@@ -70,7 +90,9 @@ public final class Parser
         double gravity = Double.parseDouble(gravitys);
         return gravity;
     }
-
+    /**
+     * metoda pobierająca dane o maksymalnej prędkości Y
+     */
     public static double getMaxyvel(){
         Properties properties = loadProperties();
         System.setProperty("file.encoding", "UTF-8");
@@ -78,7 +100,9 @@ public final class Parser
         double maxyvel = Double.parseDouble(maxyvels);
         return maxyvel;
     }
-
+    /**
+     * metoda pobierająca dane o maksymalnej prędkości X
+     */
     public static double getMaxxvel(){
         Properties properties = loadProperties();
         System.setProperty("file.encoding", "UTF-8");
@@ -86,7 +110,9 @@ public final class Parser
         double maxxvel = Double.parseDouble(maxxvels);
         return maxxvel;
     }
-
+    /**
+     * metoda pobierająca dane o sile silnika głównego
+     */
     public static double getMaineng(){
         Properties properties = loadProperties();
         System.setProperty("file.encoding", "UTF-8");
@@ -94,7 +120,9 @@ public final class Parser
         double mainegn = Double.parseDouble(mainegns);
         return mainegn;
     }
-
+    /**
+     * metoda pobierająca dane o sile silnika bocznego
+     */
     public static double getSideeng(){
         Properties properties = loadProperties();
         System.setProperty("file.encoding", "UTF-8");
@@ -102,7 +130,9 @@ public final class Parser
         double sideegn = Double.parseDouble(sideegns);
         return sideegn;
     }
-
+    /**
+     * metoda pobierająca dane o rozmiarze statku
+     */
     public static int getShipsize(){
         Properties properties = loadProperties();
         System.setProperty("file.encoding", "UTF-8");
@@ -110,7 +140,9 @@ public final class Parser
         int shipsize = Integer.parseInt(shipsizes);
         return shipsize;
     }
-
+    /**
+     * metoda pobierająca dane o początkowej ilości paliwa
+     */
     public static int getStartfuel(){
         Properties properties = loadProperties();
         System.setProperty("file.encoding", "UTF-8");
@@ -118,7 +150,10 @@ public final class Parser
         int startfuel = Integer.parseInt(startfuels);
         return startfuel;
     }
-
+    /**
+     * metoda pobierająca dane o tym jak szybko tracimy paliwo używając
+     * silników bocznych
+     */
     public static int getFuelconsy(){
         Properties properties = loadProperties();
         System.setProperty("file.encoding", "UTF-8");
@@ -126,7 +161,10 @@ public final class Parser
         int fuelconsy = Integer.parseInt(fuelconsys);
         return fuelconsy;
     }
-
+    /**
+     * metoda pobierająca dane o tym jak szybko tracimy paliwo używając
+     * silnika głównego
+     */
     public static int getFuelconsx(){
         Properties properties = loadProperties();
         System.setProperty("file.encoding", "UTF-8");
@@ -134,7 +172,9 @@ public final class Parser
         int fuelconsx = Integer.parseInt(fuelconsxs);
         return fuelconsx;
     }
-
+    /**
+     * metoda pobierająca dane o ilości żyć
+     */
     public static int getLives(){
         Properties properties = loadProperties();
         System.setProperty("file.encoding", "UTF-8");
@@ -142,7 +182,9 @@ public final class Parser
         int lives = Integer.parseInt(livess);
         return lives;
     }
-
+    /**
+     * metoda pobierająca współrzędne Y
+     */
     public static double[] getYval(String level) {
         Properties properties = loadProperties();
         System.setProperty("file.encoding", "UTF-8");
@@ -150,7 +192,9 @@ public final class Parser
         double[] yval = Arrays.stream(ys).mapToDouble(Double::parseDouble).toArray();
         return yval;
     }
-
+    /**
+     * metoda pobierająca współrzędne statku w zależności od poziomu trudności
+     */
     public static int[] getShipcoords(String level, String diff){
         Properties properties = loadProperties();
         System.setProperty("file.encoding", "UTF-8");
@@ -158,7 +202,9 @@ public final class Parser
         int[] shipcoords = Arrays.stream(ss).mapToInt(Integer::parseInt).toArray();
         return shipcoords;
     }
-
+    /**
+     * metoda pobierająca współrzędne bonusa, czyli dodatkowego życia
+     */
     public static int[] getBonuscoords(String level){
         Properties properties = loadProperties();
         System.setProperty("file.encoding", "UTF-8");
@@ -171,7 +217,9 @@ public final class Parser
         }
         return bonuscoords;
     }
-
+    /**
+     * metoda pobierająca współrzędne lądowiska
+     */
     public static int[] getSpotcoords(String level, String num){
         Properties properties = loadProperties();
         System.setProperty("file.encoding", "UTF-8");
@@ -179,7 +227,9 @@ public final class Parser
         int[] spotcoords = Arrays.stream(sc).mapToInt(Integer::parseInt).toArray();
         return spotcoords;
     }
-
+    /**
+     * metoda pobierająca rozmiar lądowiska
+     */
     public static int getSpotsize(String level, String num){
         Properties properties = loadProperties();
         System.setProperty("file.encoding", "UTF-8");
@@ -187,7 +237,9 @@ public final class Parser
         int spotsize = Integer.parseInt(ssize);
         return spotsize;
     }
-
+    /**
+     * metoda pobierająca punktację za każde lądowisko
+     */
     public static int getSpotpoints(String level, String num){
         Properties properties = loadProperties();
         System.setProperty("file.encoding", "UTF-8");
